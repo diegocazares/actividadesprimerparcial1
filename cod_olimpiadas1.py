5710fcca7c8c499a03c1de56cd042f990506a190
diegocazares <145036250+diegocazares@users.noreply.github.com>
2023-09-21 09:25:59 -0600

    Create cod_olimpiadas1.py

---
+++ cod_olimpiadas1.py
@@ -0,0 +1,27 @@
+def suma(a,b):
+  x=a + b
+  return x
+def resta(a,b):
+  x=a - b
+  return x
+print ("Dame el primer numero")
+a= int(input())
+print("Dame el segundo numero")
+b= int(input())
+print()
+
+print("Dame el segundo numero")
+b= int(input())
+int=("Tus resultados son")
+
+def conversion(kilometros)
+  metros=kilometros * 1000  1 kilometro=1000 metros
+return metros
+kilometros_usuario = float(input("Ingrese la cantidad en kilometros recorridos")
+metros_equivalentes = conversion(kilometros_usuario)
+print(f")
+
+def area_triangulo(base,altura):
+area=(base*altura)/2
+return area
+base
